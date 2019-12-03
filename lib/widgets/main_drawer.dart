b15c8d67981b04ca03984742170c4c192f36f39c
Welliton Klein <welliton.fokushima@gmail.com>
2019-12-03 00:07:54 -0200

    Adicionando uma gaveta personalizada

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTitle(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          buildListTitle('Meals', Icons.restaurant),
+          const SizedBox(height: 20),
+          buildListTitle('Filters', Icons.settings),
+        ],
+      ),
+    );
+  }
+}
