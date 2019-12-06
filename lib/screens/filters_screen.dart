b0130ce798063cd03a160801a1fd0b91d14c661c
Welliton Klein <welliton.fokushima@gmail.com>
2019-12-06 12:10:06 -0200

    Adicionando opçoes de filtros

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,8 +2,32 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  Widget _buildSwitchListTitle({
+    String title,
+    String subTitle,
+    bool currentValue,
+    Function updateValue,
+  }) {
+    return SwitchListTile(
+      title: Text(title),      
+      subtitle: Text(subTitle),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +36,62 @@ class FiltersScreen extends StatelessWidget {
         title: const Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: const Text('Filters!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildSwitchListTitle(
+                  title: 'Fluten-free',
+                  subTitle: 'Only include gluten-free meals.',
+                  currentValue: _glutenFree,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTitle(
+                  title: 'Lactose-free',
+                  subTitle: 'Only include lactose-free meals.',
+                  currentValue: _lactoseFree,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTitle(
+                  title: 'Vegetarian',
+                  subTitle: 'Only include vegetarian meals.',
+                  currentValue: _vegetarian,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTitle(
+                  title: 'Vegan',
+                  subTitle: 'Only include vegan meals.',
+                  currentValue: _vegan,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
